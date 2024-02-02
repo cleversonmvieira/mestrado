a2106abcc14a1524d37fdc63f144023247e48e17
Cleverson Marques Vieira <92867963+cleversonmvieira@users.noreply.github.com>
2024-02-02 15:10:53 -0300

    Update cam.py

--- xai/cam.py
+++ xai/cam.py
@@ -14,9 +14,10 @@ tracemalloc.start()
 tpi = time.time()
 
 # ----------------------------------------------------------------
-# Parâmetros
+# Parâmetros - Ajustar de acordo com o padrão esperado pela CNN
 # ----------------------------------------------------------------
 resolution = 224
+#resolution = 229
 
 # ----------------------------------------------------------------
 # Método que obtém o nome do último layer convolucional da CNN
@@ -48,32 +49,23 @@ def get_last_dense_layer(model):
     else:
         return last_dense_layer_name    
 
+
+
 # ----------------------------------------------------------------
 # Carrega o modelo treinado (arquivo .h5) utilizando o método 
 # load_model do pacote keras.models
 # ----------------------------------------------------------------
-#model = load_model("C:/Users/cleverson.vieira/Desktop/projeto-glaucoma_bkp_25042023/projeto-glaucoma/models/final_model_vgg16.h5")
-#model = load_model("C:/Users/cleverson.vieira/Desktop/projeto-glaucoma_bkp_25042023/projeto-glaucoma/models/final_model_vgg19.h5")
-#model = load_model("C:/Users/cleverson.vieira/Desktop/projeto-glaucoma_bkp_25042023/projeto-glaucoma/models/final_model_inceptionv3.h5")
-#model = load_model("C:/Users/cleverson.vieira/Desktop/projeto-glaucoma_bkp_25042023/projeto-glaucoma/models/final_model_densenet.h5")
-#model = load_model("C:/Users/cleverson.vieira/Desktop/projeto-glaucoma_bkp_25042023/projeto-glaucoma/models/final_model_xceptionnet.h5")
-model = load_model("C:/Users/cleverson.vieira/Desktop/projeto-glaucoma_bkp_25042023/projeto-glaucoma/models/final_model_resnet50.h5")
+model = load_model("Caminho para carregar o modelo treinado (.h5)")
 
 # ----------------------------------------------------------------
 # Carrega a imagem de exemplo, converte o padrão de cor (RGB) 
 # e redimensiona a imagem para o tamanho esperado pelo modelo
 # ---------------------------------------------------------------- 
-img = cv2.imread('C:/Users/cleverson.vieira/Desktop/projeto-glaucoma_bkp_25042023/projeto-glaucoma/data/acrima/validation/positive/5826.jpg')
-#img = cv2.imread('C:/Users/cleverson.vieira/Desktop/projeto-glaucoma_bkp_25042023/projeto-glaucoma/data/acrima/validation/positive/0004.jpg')
+img = cv2.imread('Caminho para carregar a imagem de entrada')
+
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 img = cv2.resize(img, (resolution,resolution), interpolation = cv2.INTER_CUBIC)
 img_orig = img.copy()
-# Removendo canal AZUL 
-#img[:,:,0] = 0  
-# Removendo canal VERDE (comentado)
-#img[:,:,1] = 0  
-# Removendo canal VERMELHO 
-#img[:,:,2] = 0  
 
 # ----------------------------------------------------------------
 # Converte a imagem de exemplo para um array numpy, adiciona
@@ -124,7 +116,7 @@ tpf = time.time()
 
 tpt = tpf-tpi
 
-print("Memória utilizada: ", tracemalloc.get_traced_memory())
+#print("Memória utilizada: ", tracemalloc.get_traced_memory())
 print("Tempo de processamento: ", tpt)
 tracemalloc.stop()
 # ----------------------------------------------------------------
@@ -132,13 +124,13 @@ tracemalloc.stop()
 # ativação da classe (CAM)
 # ----------------------------------------------------------------
 fig, (ax1, ax2, ax3) = plt.subplots(1, 3, figsize=(20,20))
-ax1.set_title("Input")
+ax1.set_title("Imagem de Entrada")
 ax1.imshow(img_orig)
 ax1.axis('off')
 ax2.set_title("CAM")
 ax2.imshow(heatmap)
 ax2.axis('off')
-ax3.set_title("Output")
+ax3.set_title("Imagem de Saída")
 ax3.imshow(cam_img)
 ax3.axis('off')
 plt.show()
